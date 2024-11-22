53dd308310862410cf556ab41aad0eaacc501bbb
Aaron Medina <squagnito@gmail.com>
2024-11-22 16:14:17 -0600

    Initial Commit

---
+++ DailyHabit/DailyHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHabitApp.swift
+//  DailyHabit
+//
+//  Created by Aaron Medina on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
